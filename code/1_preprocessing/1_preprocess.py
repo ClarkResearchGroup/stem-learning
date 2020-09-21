c9222814d6173495ae55044ce7af90ac1240dda3
Abid Khan <aakhan3@illinois.edu>
2020-09-21 16:20:35 -0500

    only 1 test file will be constructed

--- code/1_preprocessing/1_preprocess.py
+++ code/1_preprocessing/1_preprocess.py
@@ -1,11 +1,10 @@
 from make_data import *
-import numpy as np
 
-input_dir = "../../data/WSeTe/simulated/"
+input_dir  = "../../data/WSeTe/simulated/"
 train_dirs = ["0", "1", "2"]
-test_dirs  = ["3"]
+test_dir   = ["3"]
 label_list = ["Se"]
-parsed_dir_name='parsed_label_Se'
+parsed_dir_name = 'parsed_label_Se'
 ftype = '.tiff'
 
 l_shape = (256,256)
@@ -21,11 +20,12 @@ ones_percent = 0.
 
 create_augments(input_dir, train_dirs, ftype)
 
-make_data(input_dir, train_dirs, label_list, l_shape, stride, ftype parsed_dir_name=parsed_dir_name, \
+make_data(input_dir, train_dirs, label_list, l_shape, stride, ftype, parsed_dir_name=parsed_dir_name, \
         prefix="train", AUG=True, tol=tol, ones_pcent=ones_percent, one_save=one_pickle, fsize=tr_fsize)
-make_data(input_dir, test_dirs, label_list, l_shape, stride, ftype, parsed_dir_name=parsed_dir_name, \
-        prefix="test", AUG=False, tol=tol, ones_pcent=ones_percent, one_save=one_pickle, fsize=ts_fsize)
+make_data(input_dir, test_dir, label_list, l_shape, stride, ftype, parsed_dir_name=parsed_dir_name, \
+        prefix="test", AUG=False, tol=tol, ones_pcent=ones_percent, one_save=True, fsize=ts_fsize)
 
+import numpy as np
 parsed_fn = input_dir + parsed_dir_name + "/test_00000.p"
 check_data(parsed_fn, l_shape=l_shape)
 
